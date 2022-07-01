11e46a844ac7fc24f55112b5d5a7eac99f4670c8
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-07-01 17:07:14 +0100

    backend setup completed

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
-import express  from "express";
+import express from "express";
 import mongoose from 'mongoose'
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
 
 const app = express();
+
+app.use(express.json())
+
+app.use("/api/user",router);
+app.use("/api/blog", blogRouter);
 
 mongoose.connect('mongodb+srv://admin:eCvAbqWB2ZP5RoKx@cluster0.lucam.mongodb.net/Blog?retryWrites=true&w=majority')
 .then(() => app.listen(5000))
